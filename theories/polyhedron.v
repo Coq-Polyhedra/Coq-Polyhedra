cdbeb188a0847b0ea68a7a857fd77d59039ac951
Xavier Allamigeon <xavier.allamigeon@inria.fr>
2018-07-02 12:58:48 +0200

    Clean old material.

--- theories/polyhedron.v
+++ theories/polyhedron.v
@@ -219,6 +219,9 @@ Admitted.
 
 End Lift.
 
+Arguments is_included_in_hyperplaneP [R n P c d].
+Prenex Implicits is_included_in_hyperplaneP.
+
 Section Base.
 
 Variable R : realFieldType.
@@ -261,7 +264,10 @@ Notation "{ 'eq' P }" := (active P _).
 
 Lemma active_inP (P: 'poly[R]_n) (m: nat) (A:'M[R]_(m,n)) (b: 'cV[R]_m) i :
   reflect (forall x, x \in P -> (A *m x) i 0 = b i 0) (i \in { eq(P) on 'P(A,b) }).
-Admitted.
+Proof.
+rewrite inE; apply/(equivP is_included_in_hyperplaneP).
+by split; move => H x; move/(_ x): H; rewrite row_vdot.
+Qed.
 
 Arguments active_inP [P m A b].
 Prenex Implicits active_inP.
@@ -270,7 +276,7 @@ Variable P : 'poly[R]_n.
 Variable m : nat.
 Variable A : 'M[R]_(m,n).
 Variable b : 'cV[R]_m.
-Hypothesis P_base : has_base P 'P(A,b).
+Hypothesis P_base : [ P has \base 'P(A,b) ].
 
 Lemma activeP I :
   P = '['P^=(A, b; I)] -> I \subset { eq P }.
@@ -304,217 +310,7 @@ End Base.
 
 Notation "{ 'eq' P 'on' base }" := (active P base).
 Notation "{ 'eq' P }" := (active P _).
-
-Check hpoly_of_baseP.
-
-End EquivRel.
-
-Arguments hpoly_equiv_rel [R n].
-
-Definition poly R n := {eq_quot (@hpoly_equiv_rel R n)}%qT.
-
-Notation "''poly[' R ]_ n" := (@poly R n).
-Notation "''poly_' n" := ('poly[_]_n).
-Notation "\poly" := (\pi_('poly_ _))%qT.
-Notation "''[' P ]" := (\pi_('poly_ _) P)%qT.
-
-Definition mem_polyhedron (R : realFieldType) (n : nat) : 'poly[R]_n -> pred_class :=
-  lift_fun1 'poly[R]_n id.
-Coercion mem_polyhedron : poly >-> pred_class.
-
-Section Lift.
-
-Variable R : realFieldType.
-Variable n : nat.
-
-CoInductive hpoly_spec (P : 'poly[R]_n) : 'hpoly[R]_n -> Type :=
-  HpolySpec Q of (P = '[Q]) : hpoly_spec P Q.
-
-Lemma hpolyP (P : 'poly[R]_n) :
-  hpoly_spec P (hpoly P).
-Proof.
-by constructor; rewrite hpolyK.
-Qed.
-
-Lemma ext_eqquotP (P Q : 'hpoly[R]_n) :
-  (P =i Q) <-> (P =e Q).
-Proof.
-by split => [? |]; [ apply/eqquotP/ext_eqP | move/eqquotP/ext_eqP].
-Qed.
-
-Definition has_base (P : 'poly[R]_n) (base : 'hpoly[R]_n) :=
-  [exists Q : 'hpolyEq(base), P == '[Q]].
-
-Lemma has_baseP (P : 'poly[R]_n) (base : 'hpoly[R]_n) :
-  reflect (exists (Q : 'hpolyEq(base)), P = '[Q]) (has_base P base).
-Proof.
-exact: exists_eqP.
-Qed.
-
-Lemma repr_hpolyEq (P : 'poly[R]_n) (Q : 'hpoly[R]_n) :
-  P = '[Q] -> P = '['P^=(Q; set0)].
-Proof.
-case: Q => [m A b] ->.
-apply/ext_eqquotP => x.
-rewrite hpolyEq_inE.
-suff ->: [forall j in set0, (A *m x) j 0 == b j 0]
-  by rewrite andbT.
-by apply/forall_inP => j; rewrite inE.
-Qed.
-
-Lemma self_base (P : 'poly[R]_n) (Q : 'hpoly[R]_n) :
-  P = '[Q] -> has_base P Q.
-Proof.
-move/repr_hpolyEq => P_eq.
-by apply/existsP; exists 'P^=(Q; set0); rewrite P_eq.
-Qed.
-
-Lemma mem_polyhedron_quotP (x : 'cV[R]_n) :
-  { mono \poly : P / x \in P >-> (x \in mem_polyhedron P) }.
-Proof.
-unlock mem_polyhedron => P /=.
-by case: (hpolyP '[ P ]) => Q /ext_eqquotP.
-Qed.
-
-Canonical mem_polyhedron_quot x := Eval hnf in PiMono1 (mem_polyhedron_quotP x).
-Canonical polyhedron_predType := Eval hnf in @mkPredType 'cV[R]_n 'poly[R]_n (@mem_polyhedron R n).
-
-Definition non_empty := lift_fun1 'poly[R]_n (@HPrim.non_empty R n).
-
-Lemma non_empty_quotP :
-  { mono \poly : P / HPrim.non_empty P >-> non_empty P }.
-Proof.
-unlock non_empty => P /=.
-case: (hpolyP '[ P ]) => Q /ext_eqquotP P_eq_i_Q.
-apply/idP/idP => /HPrim.non_emptyP [x H];
-  apply/HPrim.non_emptyP; exists x; by rewrite ?P_eq_i_Q in H *.
-Qed.
-
-Canonical non_empty_quot := Eval hnf in PiMono1 non_empty_quotP.
-
-Implicit Type c : 'cV[R]_n.
-
-Definition bounded c := lift_fun1 'poly[R]_n (HPrim.bounded c).
-Definition unbounded c := lift_fun1 'poly[R]_n (HPrim.unbounded c).
-Definition opt_value c := lift_fun1 'poly[R]_n (HPrim.opt_value c).
-
-CoInductive lp_state c (P : 'poly[R]_n) : option R -> bool -> bool -> bool -> Type :=
-| Empty of P =i pred0 : lp_state c P None false false false
-| Bounded (v : R) of (exists x, x \in P /\ '[c,x] = v) * (forall x, x \in P -> v <= '[c,x]) : lp_state c P (Some v) true true false
-| Unbounded of (forall K, exists x, x \in P /\ '[c,x] < K) : lp_state c P None true false true.
-
-Lemma lp_stateP c P :
-  lp_state c P (opt_value c P) (non_empty P) (bounded c P) (unbounded c P).
-Proof. (* RK *)
-unlock opt_value non_empty bounded unbounded; rewrite /HPrim.opt_value.
-case: (HPrim.lp_stateP c (hpoly P)) =>
-  [empty_hpoly_P | x [x_in_hpoly_P x_is_opt] | unbounded_hpoly_P].
-- constructor.
-  move => x.
-  by rewrite -{1}[P]reprK (mem_polyhedron_quotP x (hpoly P)) empty_hpoly_P.
-- constructor.
-  split.
-  + exists x.
-    split; last by done.
-      by rewrite -{1}[P]reprK (mem_polyhedron_quotP x (hpoly P)).
-  + move => y.
-    rewrite -{1}[P]reprK (mem_polyhedron_quotP y (hpoly P)).
-    exact: x_is_opt.
-- constructor.
-  move => K.
-  move: (unbounded_hpoly_P K) => [x ?].
-  exists x.
-  by rewrite -{1}[P]reprK (mem_polyhedron_quotP x (hpoly P)).
-Qed.
-
-Variable c : 'cV[R]_n.
-
-Lemma bounded_quotP :
-  (* TODO: we should prove all the quotP statements in a row,
-           using the (h)lp_stateP statement *)
-  { mono \poly : P / HPrim.bounded c P >-> bounded c P }.
-Proof.
-unlock bounded => P /=.
-case: (hpolyP '[ P ]) => Q.
-move => /ext_eqquotP P_eq_Q.
-case: (HPrim.lp_stateP c P); case: (HPrim.lp_stateP c Q); try by done.
-- move => z [z_in_Q _] /(_ z).
-  by rewrite P_eq_Q z_in_Q inE.
-- move => Q_empty z [z_in_Q _]; move/(_ z): Q_empty.
-  by rewrite -P_eq_Q z_in_Q inE.
-- move => Q_unbounded z [z_in_P z_opt].
-  move/(_ '[c,z]): Q_unbounded => [x [x_in_Q x_lt_z]].
-  rewrite -P_eq_Q in x_in_Q.
-  move/(_ _ x_in_Q): z_opt => z_le_x.
-  by move/andP: (conj z_le_x x_lt_z); rewrite lter_anti.
-- move => z [z_in_Q z_opt] P_unbounded.
-  move/(_ '[c,z]): P_unbounded => [x [x_in_P x_lt_z]].
-  rewrite P_eq_Q in x_in_P.
-  move/(_ _ x_in_P): z_opt => z_le_x.
-  by move/andP: (conj z_le_x x_lt_z); rewrite lter_anti.
-Qed.
-
-Canonical bounded_quot := Eval hnf in PiMono1 bounded_quotP.
-
-Lemma unbounded_quotP :
-  { mono \poly : P / HPrim.unbounded c P >-> unbounded c P }.
-Proof.
-unlock unbounded => P /=.
-case: (hpolyP '[ P ]) => Q /ext_eqquotP P_eq_Q.
-case: (HPrim.lp_stateP c P); case: (HPrim.lp_stateP c Q); try by done.
-+ move => Q_unbounded.
-  move/(_ 0): Q_unbounded => [x [x_in_Q x_lt_0]].
-  move/(_ x).
-    by rewrite P_eq_Q x_in_Q inE.
-+ move => Q_unbounded z [z_in_P z_opt].
-  move/(_ '[c,z]): Q_unbounded => [x [x_in_Q x_lt_z]].
-  rewrite -P_eq_Q in x_in_Q.
-  move/(_ _ x_in_Q): z_opt => z_le_x.
-    by move/andP: (conj z_le_x x_lt_z); rewrite lter_anti.
-+ move => Q_empty P_unbounded.
-  move/(_ 0): P_unbounded => [x [x_in_Q x_lt_0]].
-  move: (Q_empty x).
-    by rewrite -P_eq_Q x_in_Q inE.
-+ move => z [z_in_Q z_opt] P_unbounded.
-  move/(_ '[c,z]): P_unbounded => [x [x_in_P x_lt_z]].
-  rewrite P_eq_Q in x_in_P.
-  move/(_ _ x_in_P): z_opt => z_le_x.
-    by move/andP: (conj z_le_x x_lt_z); rewrite lter_anti.
-Qed.
-
-Canonical unbounded_quot := Eval hnf in PiMono1 unbounded_quotP.
-
-Lemma opt_value_quotP :
-  { mono \poly : P / HPrim.opt_value c P >-> opt_value c P }.
-Proof.
-unlock opt_value => P /=.
-case: (hpolyP '[ P ]) => Q /ext_eqquotP P_eq_Q.
-rewrite /HPrim.opt_value.
-case: (HPrim.lp_stateP c P); case: (HPrim.lp_stateP c Q) => /=; try by done.
-+ move => z [z_in_Q _] /(_ z).
-    by rewrite (P_eq_Q z) z_in_Q inE.
-+ move => Q_empty z [z_in_P _].
-    by rewrite (P_eq_Q z) (Q_empty z) inE in z_in_P.
-+ move => z [z_in_Q z_opt_in_Q].
-  move => y [y_in_P y_opt_in_P].
-  apply/congr1/eqP; rewrite eqr_le; apply/andP; split.
-  * rewrite P_eq_Q in y_in_P; exact: z_opt_in_Q.
-  * rewrite -P_eq_Q in z_in_Q; exact: y_opt_in_P.
-+ move => Q_unbounded z [z_in_P z_opt].
-  move/(_ '[c,z]): Q_unbounded => [x [x_in_Q x_lt_z]].
-  rewrite -P_eq_Q in x_in_Q.
-  move/(_ _ x_in_Q): z_opt => z_le_x.
-    by move/andP: (conj z_le_x x_lt_z); rewrite lter_anti.
-+ move => z [z_in_Q z_opt] P_unbounded.
-  move/(_ '[c,z]): P_unbounded => [x [x_in_P x_lt_z]].
-  rewrite P_eq_Q in x_in_P.
-  move/(_ _ x_in_P): z_opt => z_le_x.
-    by move/andP: (conj z_le_x x_lt_z); rewrite lter_anti.
-Qed.
-
-Canonical opt_value_quot := Eval hnf in PiMono1 opt_value_quotP.
-
-End Lift.
+Notation "[ P 'has' '\base' base ]" := (has_base P base).
 
 Section Face.
 
