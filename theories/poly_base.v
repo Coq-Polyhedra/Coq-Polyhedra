bc86689deb3c9bf9b54ceab9c4b833510ff9d5ff
Xavier Allamigeon <xavier.allamigeon@inria.fr>
2020-10-13 18:01:28 +0200

    Clean with Quentin.

--- theories/poly_base.v
+++ theories/poly_base.v
@@ -978,60 +978,67 @@ Context {base : base_t[R,n]} {P : {poly base}}.
 
 Hypothesis (Pneq0 : P `>` [poly0]).
 
-
-Lemma inactive_pt (e : base):
-  (fsval e) \notin ({eq P} : {fset _}) -> exists x, ((x \in P) && (x \notin [hp (fsval e)])).
+Lemma inactive_pt e :
+  e \in (base `\` {eq P})%fset -> exists x, ((x \in P) && (x \notin [hp e])).
 Admitted.
 
-Definition mk_inactive_pt (e : base):=
-  match @idP ((fsval e) \notin ({eq P} : {fset _})) with
+Definition mk_inactive_pt e :=
+  match @idP (e \in (base `\` {eq P})%fset) with
   | ReflectT H => xchoose (inactive_pt H)
-  | _ => xchoose (proper0P Pneq0)
+  | _ => xchoose (proper0P Pneq0) (* replace by 0 *)
   end.
 
-Lemma mk_inactive_ptP (e : base) :
-  (fsval e) \notin ({eq P} : {fset _}) -> '[(fsval e).1, mk_inactive_pt e] > (fsval e).2.
+Lemma mk_inactive_ptP e :
+  e \in (base `\` {eq P})%fset -> (mk_inactive_pt e) \notin [hp e].
 Proof.
 move=> e_inactive; rewrite /mk_inactive_pt.
 case: {-}_/idP; [|rewrite e_inactive //].
 move=> e_inactive2; move: (xchooseP (inactive_pt e_inactive2)).
 set x:=(xchoose (inactive_pt e_inactive2)).
-case/andP.
-move/poly_subsetP:(poly_base_subset_hs P (fsvalP e)) => P_hse.
-by move/P_hse => ?; rewrite notin_hp.
+by case/andP.
 Qed.
 
-Lemma combine_hs_hp  (w : {convex 'cV[R]_n ~> R}) (e : base) :
-  {subset (finsupp w) <= [hs (fsval e)]} -> combine w \in [hp (fsval e)] ->
-  {subset (finsupp w) <= [hp (fsval e)]}.
+Lemma vdot_sumDr (I: eqType) r (Q: pred I) (F : I -> 'cV[R]_n) x : '[x, \sum_(i <- r | Q i) F i] = \sum_(i <- r | Q i) '[x, F i ].
 Proof.
-move => dom_hs_e combine_hp x x_finsupp.
-apply: contraT.
-move: (dom_hs_e _ x_finsupp) => x_hs; rewrite (notin_hp x_hs).
-rewrite combineE in_hp in combine_hp.
-move/ltr_pmul2l: (gt0_fconvex x_finsupp) => <-; rewrite -vdotZr.
-have:
-  forall (y:finsupp w), (w (fsval y)) * (fsval e).2 <=
-  '[(w (fsval y)) *: (fsval y), (fsval e).1].
-- move=> y; move: (fsvalP y)=> y_finsupp.
-  move: (dom_hs_e _ y_finsupp); rewrite in_hs vdotZl vdotC.
-  by rewrite (ler_pmul2l (gt0_fconvex y_finsupp)).
-move=> finsupp_ger x_gtr.
-have: exists z:finsupp w,
-  w (fsval z) * (fsval e).2 <
-  '[w (fsval z) *: fsval z, (fsval e).1].
-- admit.
-move/sumr_ltrP: finsupp_ger => /= app; move/app.
-rewrite vdotC vdot_sumDl in combine_hp.
-move/eqP: combine_hp => ->.
-by rewrite -mulr_suml -weightE wgt1_fconvex mul1r lt_irreflexive.
-Admitted.
+apply: (big_morph (fun y => '[x,y])).
+- move => y y'; exact: vdotDr.
+- exact: vdot0r.
+Qed.
 
+Lemma combine_hs_hp  (w : {convex 'cV[R]_n ~> R}) (e : lrel[R]_n) :
+  {subset (finsupp w) <= [hs e]} -> combine w \in [hp e] ->
+  {subset (finsupp w) <= [hp e]}.
+Proof.
+set x0 := combine w.
+move => supp_sub_hs x0_in x x_in.
+move: x0_in; apply: contraTT => x_notin_hp.
+have x_inactive: '[e.1, x] > e.2.
+- rewrite -notin_hp //.
+  by apply/supp_sub_hs.
+rewrite /x0 combineE in_hp vdot_sumDr.
+rewrite eq_sym; apply/ltr_neq.
+have ->: e.2 = \sum_(y : finsupp w) (w (fsval y)) * e.2.
+- by rewrite -mulr_suml -weightE wgt1_fconvex mul1r.
+apply/sumr_ltrP => [y|].
+- move/(_ _ (fsvalP y)): supp_sub_hs; rewrite in_hs.
+  rewrite vdotZr ler_pmul2l //.
+  by apply/gt0_fconvex.
+- exists [` x_in]%fset.
+  rewrite vdotZr /=; rewrite ltr_pmul2l //.
+  by apply/gt0_fconvex.
+Qed.
 
-Definition finsupp_relint := [fset (mk_inactive_pt e) | e : base ]%fset.
+(* change base to base `\` {eq P} *)
+Definition finsupp_relint := (mk_inactive_pt @` base)%fset.
 Variable w : {convex 'cV[R]_n ~> R}.
-Definition relint_pt (_ : finsupp w = finsupp_relint):=
-  combine w.
+Hypothesis w_supp : finsupp w = finsupp_relint.
+
+(*Definition relint_pt :=
+  match @idP (base `\` {eq P} != fset0)%fset with
+  | ReflectT H => combine w (* replace with (uniform H) *)
+  | ReflectF _ => ppick P
+  end.
+ *)
 
 (*Definition relint_pt := \sum_(e : base) (1/#|base| mk_inactive_pt (val e).*)
 Lemma finsupp_relint_subset : {subset finsupp_relint <= P}.
@@ -1043,35 +1050,25 @@ case: {-}_/idP.
 - by move: (xchooseP (proper0P Pneq0)).
 Qed.
 
-
-Lemma relint_pt_poly (dom_w: finsupp w = finsupp_relint) :
-  (relint_pt dom_w) \in P.
+Lemma relint_pt_poly :
+  (combine w) \in P. (* relint_pt \in P *)
 Proof.
 apply: convexW; first by exact: mem_poly_convex.
-by rewrite dom_w; exact : finsupp_relint_subset.
+by rewrite w_supp; exact : finsupp_relint_subset.
 Qed.
 
-Lemma relint_ptP (dom_w: finsupp w = finsupp_relint) :
-  forall e : base, (fsval e) \notin ({eq P} : {fset _}) ->
-  '[(fsval e).1 , (relint_pt dom_w)] > (fsval e).2.
+Lemma relint_ptP e :
+  e \in (base `\` {eq P})%fset -> combine w \notin [hp e].
 Proof.
-move => e e_not_eq; rewrite -notin_hp /relint_pt. 
-- have finsupp_hs:  {subset (finsupp w) <= [hs (fsval e)]}.
-  + move/(poly_base_subset_hs P)/poly_leP: (fsvalP e).
-    by move=> P_base x; rewrite dom_w; move/finsupp_relint_subset/P_base.
-  apply: contraT; move/negPn/(combine_hs_hp finsupp_hs)=> finsupp_hp.
-  move: (mk_inactive_ptP e_not_eq).
-  have pt_e_finsupp: (mk_inactive_pt e) \in finsupp w.
-  + by rewrite dom_w; apply/imfsetP => /=; exists e.
-  set xe:= mk_inactive_pt e.
-  move: (finsupp_hs xe pt_e_finsupp) (finsupp_hp xe pt_e_finsupp) => xe_hs xe_hp.
-  by rewrite -notin_hp ?xe_hs ?xe_hp.
-- apply: convexW; first by exact: mem_poly_convex.
-  rewrite dom_w => x /finsupp_relint_subset.
-  by move/(poly_base_subset_hs P): (fsvalP e) => /poly_leP P_sub_hs /P_sub_hs.
+move => e_in.
+have e_in_base: e \in base by move: e_in; apply/fsubsetP/fsubsetDl.
+move/(_ _ e_in): mk_inactive_ptP.
+apply/contraNN => relint_pt_in.
+apply/(combine_hs_hp _ relint_pt_in).
+- move => ?; rewrite w_supp => /finsupp_relint_subset.
+  by apply/poly_leP/poly_base_subset_hs.
+- by rewrite w_supp in_imfset.
 Qed.
-
-
 
 End Relint.
 
